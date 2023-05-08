69043cddaa861a833c06e4f6ad3027bf505d30f4
Me <organizzzm@gmail.com>
2023-05-08 23:53:08 +0300

    test build

--- stories/Button.js
+++ stories/Button.js
@@ -9,7 +9,7 @@ export const createButton = ({
 }) => {
   const btn = document.createElement('button');
   btn.type = 'button xxx';
-  btn.innerHTML = `${label} <i class="icon-xxx">`;
+  btn.innerHTML = `${label} <i class="icon-xxx"><i class="icon-xxx">`;
   btn.addEventListener('click', onClick);
 
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
